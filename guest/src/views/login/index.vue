cb8a3c4cac5e746695a9e08109a19fb80ad6617d
ink <1480224563@qq.com>
2024-11-09 01:44:42 +0800

    登录页面完成

--- guest/src/views/login/index.vue
+++ guest/src/views/login/index.vue
@@ -1,18 +1,196 @@
 <script setup lang="ts">
+import { onMounted, reactive, watch } from 'vue';
+import CryptoJS from 'crypto-js';
+import { Hide, View,Message,Lock } from '@element-plus/icons-vue'
+import Qq_color from "@/components/im_login/qq_color.vue";
+
+
+const form = reactive({
+    email: "",
+    password: "",
+    rememberMe: false,
+    showPassword: false
+});
+
+onMounted(() => {
+    // 检查 localStorage 中是否有凭证
+    if (form.email) {
+        checkRememberMe();
+    }
+});
+watch(() => form.email, () => {
+    checkRememberMe();
+});
+
+const togglePasswordVisibility = () => {
+    form.showPassword = !form.showPassword;
+};
+
+const checkRememberMe = () => {
+    if (form.email) {
+        const rememberMe = localStorage.getItem(`rememberMe_${form.email}`) === 'true';
+        if (rememberMe) {
+            const encryptedEmail = localStorage.getItem(`email_${form.email}`);
+            const encryptedPassword = localStorage.getItem(`password_${form.email}`);
+
+            if (encryptedEmail && encryptedPassword) {
+                // 解密凭证
+                const bytesEmail = CryptoJS.AES.decrypt(encryptedEmail, 'secret-key');
+                const bytesPassword = CryptoJS.AES.decrypt(encryptedPassword, 'secret-key');
+                const email = bytesEmail.toString(CryptoJS.enc.Utf8);
+                const password = bytesPassword.toString(CryptoJS.enc.Utf8);
+
+                // 自动填充表单
+                form.email = email;
+                form.password = password;
+                form.rememberMe = true;
+            }
+        }
+    }
+};
+
+const handleLogin = () => {
+    if (form.email && form.password) {
+        if (form.rememberMe) {
+            // 加密并存储凭证
+            const encryptedEmail = CryptoJS.AES.encrypt(form.email, 'secret-key').toString();
+            const encryptedPassword = CryptoJS.AES.encrypt(form.password, 'secret-key').toString();
+            localStorage.setItem(`email_${form.email}`, encryptedEmail);
+            localStorage.setItem(`password_${form.email}`, encryptedPassword);
+            localStorage.setItem(`rememberMe_${form.email}`, 'true');
+        } else {
+            // 清除凭证
+            localStorage.removeItem(`email_${form.email}`);
+            localStorage.removeItem(`password_${form.email}`);
+            localStorage.removeItem(`rememberMe_${form.email}`);
+        }
+    }
+
+};
 
 </script>
 
 <template>
-<div class="im_login">login</div>
+    <div class="im_login">
+        <div class="banner">
+          <qq_color></qq_color>
+        </div>
+        <div class="login_form">
+            <el-form :model="form">
+                <el-form-item>
+                    <el-input v-model="form.email" placeholder="邮箱" :prefix-icon="Message">
+                    </el-input>
+                </el-form-item>
+                <el-form-item class="item_password">
+                    <!-- 使用 :type 动态绑定 input 类型 -->
+                    <el-input :type="form.showPassword ? 'text' : 'password'" v-model="form.password" placeholder="密码" :prefix-icon="Lock">
+                        <!-- 添加一个 slot 来放置密码可见性切换按钮 -->
+                        <template #suffix>
+                            <i v-if="form.showPassword" style="cursor: pointer; font-size: 18px;"
+                                @click="togglePasswordVisibility"><el-icon>
+                                    <Hide />
+                                </el-icon></i>
+                            <i v-else style="cursor: pointer; font-size: 18px;" @click="togglePasswordVisibility"><el-icon>
+                                    <View />
+                                </el-icon></i>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item class="item_action">
+                    <el-checkbox v-model="form.rememberMe">记住密码</el-checkbox>
+                  <el-link :underline="false" type="primary" class="right">注册账号</el-link>
+                </el-form-item>
+                <el-form-item>
+                    <el-button style="width: 100%;" type="primary" @click="handleLogin">登录</el-button>
+                </el-form-item>
+            </el-form>
+            <div class="other_login">
+                <div class="label">第三方登录</div>
+                <div class="icons">
+                    <i class="iconfont icon-QQ"></i>
+                    <i class="iconfont icon-weixin"></i>
+                    <i class="iconfont icon-shoujitongxun"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
-.im_login{
+.im_login {
     width: 500px;
     height: 406px;
     background-color: white;
     border-radius: 10px;
     overflow: hidden;
     box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+
+    .banner {
+        height: 140px;
+        width: 100%;
+        background-color: aquamarine;
+    }
+
+    .login_form {
+        padding: 20px 60px;
+    }
+
+    .item_password {
+        margin-bottom: 4px;
+    }
+
+
+    .item_action {
+        margin-bottom: 4px;
+        position: relative;
+
+        .right {
+            position: absolute;
+            right: 0.1px;
+            color: #606266;
+            cursor: pointer;
+
+            &:hover {
+                color: #409eff;
+            }
+        }
+    }
+
+    .other_login {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .label {
+            font-size: 14px;
+            color: #aaa8a8;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            &::before,
+            &::after {
+                width: 35%;
+                height: 1px;
+                background-color: #aaa8a8;
+                content: "";
+                display: inline-flex;
+            }
+        }
+
+        .icons {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 6px;
+
+            i {
+                font-size: 36px;
+                cursor: pointer;
+                margin: 0 20px;
+            }
+        }
+    }
 }
 </style>
--- guest/vite.config.ts
+++ guest/vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
+  },
   plugins: [vue()],
   resolve: {
     alias: {
